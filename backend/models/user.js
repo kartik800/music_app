9e1d4bceb619d9c9dd3eb447d5e59ad1f6a17967
Kartik <er.kartik18102016@gmail.com>
2023-03-17 21:40:18 +0530

    added user model

---
+++ backend/models/user.js
@@ -0,0 +1,49 @@
+// importing usefull modules
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+const passwordComplexity = require("joi-password-complexity");
+
+// decalering userSchema
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  gender: { type: String, required: true },
+  month: { type: String, required: true },
+  date: { type: String, required: true },
+  name: { type: String, required: true },
+  year: { type: String, required: true },
+  likedSongs: { type: [String], default: [] },
+  playlists: { type: [String], default: [] },
+  isAdmin: { type: Boolean, default: false },
+});
+
+// creating custom method to our user model
+// to generate and return json vector
+userSchema.methods.generateAuthToken = function () {
+  // jwt.sign required two parameters (payload and sign)
+  const token = jwt.sign(
+    { _id: this._id, name: this.name, isAdmin: this.isAdmin },
+    process.env.JWTPRIVATEKEY,
+    { expiresIn: "7d" }
+  );
+  return token;
+};
+
+const validate = (user) => {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(10).required(),
+    email: Joi.string().email().required(),
+    password: passwordComplexity().required(),
+    month: Joi.string().required(),
+    date: Joi.string().required(),
+    year: Joi.string().required(),
+    gender: Joi.string().valid("male", "female", "non-binary").required(),
+  });
+  return schema.validate(user);
+};
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = { User, validate };
